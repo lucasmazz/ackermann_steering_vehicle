eb93cb908cfb9ba65f76aa84997bd95c113792a3
Lucas Mazzetto <lucasrmazzetto@gmail.com>
2024-10-04 22:54:02 -0300

    feat: Ackermann steering vehicle simulation

---
+++ launch/joystick.launch.py
@@ -0,0 +1,23 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    robot_pkg_name = "ackermann_steering_vehicle"
+
+    vehicle_params_path = os.path.join(get_package_share_directory(
+        robot_pkg_name), 'config', 'parameters.yaml')
+
+    return LaunchDescription([
+        Node(
+            package=robot_pkg_name,
+            executable='joystick_controller',
+            parameters=[vehicle_params_path],
+            output='screen'
+        ),
+    ])
